147ec5e91b9f37f63bb016857983a1e80ced7332
maria jhonson <76889255+misshappend@users.noreply.github.com>
2021-02-13 10:33:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,46 +3,51 @@ import os
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-"""@client.command()
-async def createChannel(ctx, channelName):
-	guild = ctx.guild
 
-	mbed = d.Embed(
-         title = 'succes',
-         description = "{} channel created".format(channelName)
-		)
-if ctx.author.guild_permissions.manage_channels:
-	await guild.create_text_channel(name = {}.format(channelName))
-	await ctx.send(embed*mbed)"""
- 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('peo'):
-        await message.channel.send('mujer que veo mujer que morboseo :smiling_imp:')
-
-    if message.content.startswith('chill'):
-        await message.channel.send('chill')
+    if message.content.startswith('!chill'):
+         await message.channel.send("chill")
 
     #emojis = (':neutral_face: @everyone :neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face:')
     emojis = ('@everyone')
     gif = ('https://media.discordapp.net/attachments/807053711871443024/809997406677630996/neutral_face.png?width=458&height=458')
-    
-    if message.content.startswith('!raid' , print('the raid begin')):
-         while True:
-             await message.channel.send(emojis)
-             await message.channel.send(gif)
-          
 
+    if message.content.startswith('!raid', print('the raid begin')):
+        while True:
+            await message.channel.send(emojis)
+            await message.channel.send(gif)
+
+    help = ("""```py
+!chill = chill como siempre
+
+!ayuda = informacion de los comandos del bot
+
+!sr = acaba la raid
+
+!raid = raid
+
+!SETSMS = entra a SETSMS
+
+!neofetch = muestra informacion del sistema
+
+!atack (esta en desarrollo)
+
+            ```""")
+
+    if message.content.startswith('!ayuda'):
+        await message.channel.send(help)
 
     if message.content.startswith('!sr'):
-          exit(print("raid over"))
+        exit(print("raid over"))
 
     neofetch = ("""
            ```py
@@ -73,13 +78,18 @@ async def on_message(message):
 	    """)
 
     if message.content.startswith('!neofetch'):
-    	await message.channel.send(neofetch)
+        await message.channel.send(neofetch)
 
-    if message.content.startswith('SETSMS'):
-    	test = os.system("cd SETSMS ; bash SETSMS.sh")
-    	await message.channel.send("")
+    if message.content.startswith('!SETSMS'):
+        os.system("cd SETSMS ; bash SETSMS.sh")
+        await message.channel.send("esta en SETSMS!")
 
+    numero = ("+573222222222")
     if message.content.startswith('!atack'):
-    	await message.channel.send("se hizo el ataque se demora unos 3-4 min", numero)
+        await message.channel.send(f"se ataco a : {numero} se demora unos 3-4 minutos")
+
+    if message.content.startswith('test'):
+        await message.channel.send(numero)
+        os.write(f"{numero}")
 
 client.run('')
