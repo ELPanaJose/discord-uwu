c94d4305214684ae2cfe53dbed57d8277ee31e4b
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-15 15:35:41 -0500

    Update largeC.py

--- largeC.py
+++ largeC.py
@@ -11,32 +11,4 @@ ayuda = ("""```py
 !atack (esta en desarrollo)
             ```""")
 
-
-
-
-neofetch = ("""
-           ```py
-                         host information
-                                   u0_a138@localhost
-          +hydNNNNdyh+             -----------------
-        +mMMMMMMMMMMMMm+           OS: Android 9 armv7l
-      dMMm:NMMMMMMN:mMMd           Host: motorola moto e6 play
-      hMMMMMMMMMMMMMMMMMMh         Kernel: 4.4.146+
-  ..  yyyyyyyyyyyyyyyyyyyy  ..     Uptime: 26 days, 18 hours, 5
-.mMMmMMMMMMMMMMMMMMMMMMMMmMMm.     Packages: 133 (dpkg), 1 (pkg
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Shell: bash 5.1.4
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   CPU: MT6739WW (4) @ 1.495GHz
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Memory: 1164MiB / 1862MiB
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
--MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-
- +yy+ MMMMMMMMMMMMMMMMMMMM +yy+
-      mMMMMMMMMMMMMMMMMMMm
-      /++MMMMh++hMMMM++/
-          MMMMo  oMMMM
-          MMMMo  oMMMM
-          oNMm-  -mMNs
-          
-                       ```
-	    """)
-
 numero = ("+573222222222")
