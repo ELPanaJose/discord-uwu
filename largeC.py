204306c4e3a04224a10a723d51f9df3b51b0eba4
maria jhonson <76889255+misshappend@users.noreply.github.com>
2021-02-15 01:22:24 -0500

    Create largeC.py

---
+++ largeC.py
@@ -0,0 +1,41 @@
+
+
+ayuda = ("""```py
+
+!chill = chill como siempre
+!ayuda = informacion de los comandos del bot
+!sr = acaba la raid
+!raid = raid
+!SETSMS = entra a SETSMS
+!neofetch = muestra informacion del sistema
+!atack (esta en desarrollo)
+            ```""")
+
+
+
+
+neofetch = ("""
+           ```py
+                         host information
+                                   u0_a138@localhost
+          +hydNNNNdyh+             -----------------
+        +mMMMMMMMMMMMMm+           OS: Android 9 armv7l
+      dMMm:NMMMMMMN:mMMd           Host: motorola moto e6 play
+      hMMMMMMMMMMMMMMMMMMh         Kernel: 4.4.146+
+  ..  yyyyyyyyyyyyyyyyyyyy  ..     Uptime: 26 days, 18 hours, 5
+.mMMmMMMMMMMMMMMMMMMMMMMMmMMm.     Packages: 133 (dpkg), 1 (pkg
+:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Shell: bash 5.1.4
+:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   CPU: MT6739WW (4) @ 1.495GHz
+:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Memory: 1164MiB / 1862MiB
+:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
+-MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-
+ +yy+ MMMMMMMMMMMMMMMMMMMM +yy+
+      mMMMMMMMMMMMMMMMMMMm
+      /++MMMMh++hMMMM++/
+          MMMMo  oMMMM
+          MMMMo  oMMMM
+          oNMm-  -mMNs
+          
+                       ```
+	    """)
+
