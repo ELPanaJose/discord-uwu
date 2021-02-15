4d9fa83a76ddc18313c5a6ac79dacaa333353b4e
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-15 14:58:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import discord
 import os
 from largeC import *
-
-neofetch = neofetch
-
+    
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -17,11 +15,11 @@ async def on_message(message):
 
     if message.content.startswith('!chill'):
          await message.channel.send('chill {0.author}'.format(message))
-	
+
     emojis = ('@everyone')
     gif = ('https://media.discordapp.net/attachments/807053711871443024/809997406677630996/neutral_face.png?width=458&height=458')
 
-    if message.content.startswith('!raid', print('the raid begin')):
+    if message.content.startswith('!raid', print(chr(27)+"[1;33m"+"the raid begin")):
         while True:
             await message.channel.send(emojis)
             await message.channel.send(gif)
@@ -38,9 +36,9 @@ async def on_message(message):
     if message.content.startswith('!SETSMS'):
         os.system("cd SETSMS ; bash SETSMS.sh")
         await message.channel.send("esta en SETSMS!")
-	
+
     if message.content.startswith('!atack'):
-        await message.channel.send(f"se ataco a : {numero} se demora unos 3-4 minutos")
+        await message.channel.send(f"se ataco a : ||{numero}|| se demora unos 3-4 minutos")
 
     if message.content.startswith('test'):
         await message.channel.send(numero)
