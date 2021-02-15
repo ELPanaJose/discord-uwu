860351367007f5b1aa3190e9e4b3ae814ce050d6
maria jhonson <76889255+misshappend@users.noreply.github.com>
2021-02-15 01:23:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import discord
 import os
+from largeC import *
+
+neofetch = neofetch
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -15,9 +16,8 @@ async def on_message(message):
         return
 
     if message.content.startswith('!chill'):
-         await message.channel.send("chill")
-
-    #emojis = (':neutral_face: @everyone :neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face::neutral_face:')
+         await message.channel.send('chill {0.author}'.format(message))
+	
     emojis = ('@everyone')
     gif = ('https://media.discordapp.net/attachments/807053711871443024/809997406677630996/neutral_face.png?width=458&height=458')
 
@@ -26,56 +26,11 @@ async def on_message(message):
             await message.channel.send(emojis)
             await message.channel.send(gif)
 
-    help = ("""```py
-!chill = chill como siempre
-
-!ayuda = informacion de los comandos del bot
-
-!sr = acaba la raid
-
-!raid = raid
-
-!SETSMS = entra a SETSMS
-
-!neofetch = muestra informacion del sistema
-
-!atack (esta en desarrollo)
-
-            ```""")
-
     if message.content.startswith('!ayuda'):
-        await message.channel.send(help)
+        await message.channel.send(ayuda)
 
     if message.content.startswith('!sr'):
         exit(print("raid over"))
-
-    neofetch = ("""
-           ```py
-
-                         host information
-
-
-                                   u0_a138@localhost
-          +hydNNNNdyh+             -----------------
-        +mMMMMMMMMMMMMm+           OS: Android 9 armv7l
-      dMMm:NMMMMMMN:mMMd           Host: motorola moto e6 play
-      hMMMMMMMMMMMMMMMMMMh         Kernel: 4.4.146+
-  ..  yyyyyyyyyyyyyyyyyyyy  ..     Uptime: 26 days, 18 hours, 5
-.mMMmMMMMMMMMMMMMMMMMMMMMmMMm.     Packages: 133 (dpkg), 1 (pkg
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Shell: bash 5.1.4
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   CPU: MT6739WW (4) @ 1.495GHz
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   Memory: 1164MiB / 1862MiB
-:MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
--MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-
- +yy+ MMMMMMMMMMMMMMMMMMMM +yy+
-      mMMMMMMMMMMMMMMMMMMm
-      /++MMMMh++hMMMM++/
-          MMMMo  oMMMM
-          MMMMo  oMMMM
-          oNMm-  -mMNs
-          
-                       ```
-	    """)
 
     if message.content.startswith('!neofetch'):
         await message.channel.send(neofetch)
