53f8a38a0eff361ed05a3186337230409bf74da6
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-16 10:27:38 -0500

    Update largeC.py

--- largeC.py
+++ largeC.py
@@ -2,13 +2,29 @@
 
 ayuda = ("""```py
 
-!chill = chill como siempre
-!ayuda = informacion de los comandos del bot
-!sr = acaba la raid
-!raid = raid
-!SETSMS = entra a SETSMS
-!neofetch = muestra informacion del sistema
-!atack (esta en desarrollo)
+*chill = chill como siempre
+
+*sr = acaba la raid
+
+*raid = raid
+
+*SETSMS = entra a SETSMS
+
+*atack (esta en desarrollo)
+
+*ayuda te da informacion sobre los comandos
+( for now the bot is only in spanih, but in
+a future it will be memes in english :D)
+
+*monda de solo puro momo
+
+*bruh : bruh
+
+*nm para agregar nuevos memes
+
+*cm solo te da informacion de cuantos memes tienes
+
+*meme solo te da un meme
             ```""")
 
 numero = ("+573222222222")
