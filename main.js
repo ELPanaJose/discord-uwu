54f464cccae2ca0b5e438ff0aaa8556e6bcd28d4
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-03-06 23:38:47 -0500

    Update and rename reddit.js to main.js

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 const linkMemes = [
-  "https://www.reddit.com/r/MAAU/new/",
-  "https://www.reddit.com/r/DylanteroYT/new/",
   "https://www.reddit.com/r/MAAU/top/",
   "https://www.reddit.com/r/DylanteroYT/hot/",
-  "https://www.reddit.com/r/MAAU/hot/",
-  "https://www.reddit.com/r/DylanteroYT/top/",
   "https://www.reddit.com/r/linuxmemes/hot",
-  "https://www.reddit.com/r/linuxmemes/new",
-  "https://www.reddit.com/r/linuxmemes/top"
+  "https://www.reddit.com/r/linuxmemes/new/",
+  "https://www.reddit.com/r/linuxmemes/top/",
+  "https://www.reddit.com/r/shitposting/top/",
+  "https://www.reddit.com/r/shitposting/new/",
+  "https://www.reddit.com/r/shitposting/hot/",
+  "https://www.reddit.com/r/ProgrammerHumor/hot/",
+  "https://www.reddit.com/r/ProgrammerHumor/new/",
+  "https://www.reddit.com/r/ProgrammerHumor/top/",
+  "https://www.reddit.com/r/MAAU/hot/",
+  "https://www.reddit.com/r/MAAU/new/"
 ];
 
-Object.defineProperty(Array.prototype, "flat", {
-  value: function(depth = 1) {
-    return this.reduce(function(flat, toFlatten) {
-      return flat.concat(
-        Array.isArray(toFlatten) && depth > 1
-          ? toFlatten.flat(depth - 1)
-          : toFlatten
-      );
-    }, []);
-  }
-});
+var ayuda =
+  "```*nm = nuevo meme\n*meme = te da un meme\n*cm = cantidad de memes```";
 
 var img = [];
 const Discord = require("discord.js");
@@ -29,10 +24,10 @@ const cheerio = require("cheerio");
 
 const client = new Discord.Client(),
   commands = {
-    "*nm": async function(msg) {
+    "*nm": async function (msg) {
       msg.channel.send("se busco un nuevo meme");
-      linkMemes.map(i => {
-        axios.get(i).then(r => {
+      linkMemes.map((i) => {
+        axios.get(i).then((r) => {
           let $ = cheerio.load(r.data);
           $("._2_tDEnGMLxpM6uOa2kaDB3").each((i, e) => {
             img.push($(e).attr("src"));
@@ -40,7 +35,7 @@ const client = new Discord.Client(),
         });
       });
     },
-    "*meme": async function(msg) {
+    "*meme": async function (msg) {
       let exampleEmbed = new Discord.MessageEmbed()
         .setTitle(
           ":regional_indicator_b: :regional_indicator_r:  :regional_indicator_u: :regional_indicator_h:"
@@ -48,15 +43,18 @@ const client = new Discord.Client(),
         .setImage(img[Math.floor(Math.random() * img.length)]);
       msg.channel.send(exampleEmbed);
     },
-    "*cm": async function(msg) {
+    "*cm": async function (msg) {
       msg.channel.send(`la cantidad de memes es :${img.length}`);
+    },
+    "*help": async function (msg) {
+      msg.channel.send(ayuda);
     },
   };
 
 client.on("ready", () => {
-  console.log(`reddit ready! ${client.user.tag}!`);
+  console.log(`bot ready! ${client.user.tag}!`);
 });
-client.on("message", msg => {
+client.on("message", (msg) => {
   if (commands.hasOwnProperty(msg.content)) {
     commands[msg.content](msg);
   }
